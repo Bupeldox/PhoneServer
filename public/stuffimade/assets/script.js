25e3f99d3f2814be919fe4fa16e51a07920adbeb
bupledox <bupledox@gmail.com>
2020-11-01 11:42:08 +0000

    Adding stuffimade

---
+++ public/stuffimade/assets/script.js
@@ -0,0 +1,68 @@
+function multiImageMove(selector) {
+    //move many
+    gsap.to(selector + " .image", {
+        y: function(i, t) {
+            return (i / $(t).siblings().length * 30) + "vh";
+        },
+        scrollTrigger: {
+            scrub: 0.1,
+            trigger: ".imageSection" + selector,
+            start: "top bottom",
+            end: "bottom top",
+            ease: "power1.inOut"
+                //ease: "liniar",
+                //markers:true,
+        }
+    });
+}
+
+function parallax(selector) {
+    //parallax 
+    gsap.to(selector + " .image .img", {
+        y: 40,
+        scrollTrigger: {
+            // scrub: 0.5,
+            scrub: 0.1,
+            trigger: selector + ".imageSection",
+            start: "top bottom",
+            end: "bottom top",
+            ease: "power1.inOut"
+        }
+    });
+}
+
+function initAnimations() {
+
+    multiImageMove(".s4"); //motor
+    parallax(".s4");
+    multiImageMove(".s3"); //arduinos
+    parallax(".s3");
+    parallax(".s5"); //coaster
+    multiImageMove(".s6"); //lens
+    parallax(".s6");
+    multiImageMove(".s7"); //night light
+    parallax(".s7");
+}
+
+
+// 3rd party library setup:
+const bodyScrollBar = Scrollbar.init(document.body, { damping: 0.1, renderByPixels: true });
+
+// Tell ScrollTrigger to use these proxy getter/setter methods for the "body" element: 
+ScrollTrigger.scrollerProxy(document.body, {
+    scrollTop(value) {
+        if (arguments.length) {
+            bodyScrollBar.scrollTop = value; // setter
+        }
+        return bodyScrollBar.scrollTop; // getter
+    },
+    getBoundingClientRect() {
+        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+    }
+});
+
+// when the smooth scroller updates, tell ScrollTrigger to update() too: 
+bodyScrollBar.addListener(ScrollTrigger.update);
+
+
+initAnimations();
--- server.js
+++ server.js
@@ -9,37 +9,29 @@ let Bins = {};
 let NeedToSave = false;
 
 //====Utilities
-function randomString(){
+function randomString() {
     const letters = "abcdefghijklmnopqrstuvwxyzRTYUIOPASDFGHJKLZXCVBNM";
     let output = "";
     for (let i = 0; i < 16; i++) {
-        let letter = letters[Math.floor(Math.random()*letters.length)];
-        output+=letter;
+        let letter = letters[Math.floor(Math.random() * letters.length)];
+        output += letter;
     }
     return output;
 }
 
-function getFormattedDate(){
-   return Date().split(" ").slice(1,5).join(" ");
+function getFormattedDate() {
+    return Date().split(" ").slice(1, 5).join(" ");
 }
 
 
 //====Routing
 
+
+
+//Index
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/postits/index.html');
 });
-
-app.get('/why', (req, res) => {
-    res.sendFile(__dirname + '/public/why.html');
-});
-
-app.get('/assets/:filename', (req,res) => {
-    var filename = req.params.filename;
-    res.sendFile(__dirname + '/public/assets/'+filename);
-});
-
-
 
 //Create
 app.post("/bin/create/:secret", (req, res) => {
@@ -57,9 +49,9 @@ app.post("/bin/create/:secret", (req, res) => {
 app.post("/bin/:binId", (req, res) => {
     var sentData = req.body;
     var binId = req.params.binId;
-    
+
     //Dont save urls
-    sentData = JSON.parse(JSON.stringify(sentData).replace(/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/g,"*Redacted*"));
+    sentData = JSON.parse(JSON.stringify(sentData).replace(/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/g, "*Redacted*"));
 
     if (Object.keys(sentData).length === 0) {
         res.send({ success: false, message: "Empty Object" });
@@ -69,36 +61,50 @@ app.post("/bin/:binId", (req, res) => {
         res.send({ success: false, message: "Bad id" });
         return;
     }
-    addObjectToBin(binId,sentData);
+    addObjectToBin(binId, sentData);
     res.send({ success: true });
 });
 
 //Get
-app.get("/bin/:binId",(req,res)=>{
+app.get("/bin/:binId", (req, res) => {
     var binId = req.params.binId;
-    if(!Bins.hasOwnProperty(binId)){
-        res.send({success:false,message:"bad Id"});
+    if (!Bins.hasOwnProperty(binId)) {
+        res.send({ success: false, message: "bad Id" });
         return;
     }
     var toSend = readBin(binId);
-    res.send({success:true,data:toSend});
+    res.send({ success: true, data: toSend });
 })
+
+//Assets
+app.get('/:area/assets/:filename', (req, res) => {
+    var filename = req.params.filename;
+    var area = req.params.area;
+    res.sendFile(__dirname + '/public/' + area + '/assets/' + filename);
+});
+
+//htmls
+app.get("/:area/:page", (req, res) => {
+    var area = req.params.area;
+    var page = req.params.page;
+    res.sendFile(__dirname + '/public/' + area + '/' + page + '.html');
+});
 
 
 //====Funcs
-function createBin(){
+function createBin() {
     let binId = randomString();
     Bins[binId] = [];
-    console.log("Creating Bin:" + binId +" "+ getFormattedDate());
+    console.log("Creating Bin:" + binId + " " + getFormattedDate());
     NeedToSave = true;
     return binId;
 }
 
-function readBin(binId){
-    console.log("reading bin: "+binId +" "+ getFormattedDate());
-    if(Bins.hasOwnProperty(binId)){
+function readBin(binId) {
+    console.log("reading bin: " + binId + " " + getFormattedDate());
+    if (Bins.hasOwnProperty(binId)) {
         return Bins[binId];
-    }else{
+    } else {
         return null;
     }
 }
@@ -106,38 +112,38 @@ function readBin(binId){
 function addObjectToBin(binId, object) {
     if (!object.hasOwnProperty("createdDate")) {
         object.createdDate = Date();
-    }   
+    }
     Bins[binId].push(object);
     NeedToSave = true;
-    console.log("Added:" + JSON.stringify(object) +" at "+ getFormattedDate());
+    console.log("Added:" + JSON.stringify(object) + " at " + getFormattedDate());
 }
 
 //Files
-function saveBinsToFile(){
-    if(!NeedToSave){
+function saveBinsToFile() {
+    if (!NeedToSave) {
         return;
     }
-    var path = __dirname+'/private/JsonBins.json';
+    var path = __dirname + '/private/JsonBins.json';
     console.log("Saving to :" + path);
     console.log("Saving at :" + getFormattedDate());
-    Bins.lastSaved = ""+Date();
-    
-    fs.writeFile(path,JSON.stringify(Bins),(error)=>{
-        if(error){ throw error;}else{
+    Bins.lastSaved = "" + Date();
+
+    fs.writeFile(path, JSON.stringify(Bins), (error) => {
+        if (error) { throw error; } else {
             NeedToSave = false;
         }
-        
+
     });
-    
+
 }
 
-function setBinsFromFile(){
-    fs.readFile(__dirname+'/private/JsonBins.json','utf8', function(error, data) {
-        if(error){ throw error; }
+function setBinsFromFile() {
+    fs.readFile(__dirname + '/private/JsonBins.json', 'utf8', function(error, data) {
+        if (error) { throw error; }
         console.log(data);
-        var rawData = JSON.parse(data);//{[]}
+        var rawData = JSON.parse(data); //{[]}
         Bins = rawData;
-      });
+    });
 }
 
 
@@ -145,13 +151,13 @@ function setBinsFromFile(){
 
 //===Startup
 
-function startup(){
+function startup() {
 
     setBinsFromFile();
 
     setInterval(() => {
         saveBinsToFile();
-    }, 10*60000);
+    }, 10 * 60000);
 
     console.log("setup Complete");
 }
@@ -164,5 +170,3 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
     startup();
 });
-  
-  
